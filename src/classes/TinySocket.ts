WesSouza/lgtv-ip-control

commit 116328a79b8b50e0fac8097b6fabd70f6eb44c48

Fix small TypeScript issue on TinySockets wrap function

src/classes/TinySocket.ts
@@ -80,7 +80,7 @@ export class TinySocket {
 
         if (error) {
           reject(error);
-        } else {
+        } else if (value !== undefined) {
           resolve(value);
         }
       };

commit 3611bd08c7b37d92df182444b3a6741b6388185d

Fix bug introduced by commit 116328a79b8b50e0fac8097b6fabd70f6eb44c48 where void Promise wrappers would never resolve. Also, refactor the wrapper to pass resolve and reject functions to the wrapped method rather than trying to handle all possible combinations of event parameters in the wrapper itself. This means the wrapper only has to handle error and timeout events.

src/classes/TinySocket.ts
@@ -66,22 +66,28 @@ export class TinySocket {
   }
 
   wrap<T>(
-    method: (callback: (error?: Error, value?: T) => void) => void,
+    method: (
+      resolve: (value: T) => void,
+      reject: (error: Error) => void,
+    ) => void,
   ): Promise<T> {
     return new Promise((resolve, reject) => {
-      const callback = (error?: Error, value?: T) => {
+      const cleanup = () => {
         this.client.removeListener('error', reject);
         this.client.removeListener('timeout', () => {
           reject(new TimeoutError());
         });
-
-        if (error) {
-          reject(error);
-        } else if (value !== undefined) {
-          resolve(value);
-        }
       };
-      method(callback);
+      method(
+        (value: T) => {
+          cleanup();
+          resolve(value);
+        },
+        (error: Error) => {
+          cleanup();
+          reject(error);
+        },
+      );
 
       this.client.once('error', reject);
       this.client.once('timeout', reject);
@@ -89,9 +95,9 @@ export class TinySocket {
   }
 
   connect(): Promise<void> {
-    return this.wrap((callback) => {
-      this.client.connect(this.settings.networkPort, this.host, callback);
+    return this.wrap((resolve) => {
       this.client.setTimeout(this.settings.networkTimeout);
+      this.client.connect(this.settings.networkPort, this.host, resolve);
     });
   }
 
@@ -100,16 +106,20 @@ export class TinySocket {
   }
 
   read(): Promise<Buffer> {
-    return this.wrap((callback) => {
-      this.client.once('data', (data) => {
-        callback(undefined, data);
-      });
+    return this.wrap((resolve) => {
+      this.client.once('data', resolve);
     });
   }
 
   write(data: Buffer): Promise<void> {
-    return this.wrap((callback) => {
-      this.client.write(data, callback);
+    return this.wrap((resolve, reject) => {
+      this.client.write(data, (error?: Error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
     });
   }
 
@@ -119,8 +129,8 @@ export class TinySocket {
   }
 
   disconnect(): Promise<void> {
-    return this.wrap((callback) => {
-      this.client.end(callback);
+    return this.wrap((resolve) => {
+      this.client.end(resolve);
     });
   }
 